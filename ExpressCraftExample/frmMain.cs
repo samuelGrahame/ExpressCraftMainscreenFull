f9701a28956fcdd900958d1b755c5d9fd6bcbed2
samuel grahame <samuel@intelogy.com.au>
2019-03-04 09:01:03 +1100

    Add project files.

---
+++ ExpressCraftExample/frmMain.cs
@@ -0,0 +1,27 @@
+﻿using ExpressCraft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressCraftExample
+{
+    public class frmMain : Form
+    {
+        public frmMain()
+        {
+            this.Body.SetBoundsFull();
+            this.Heading.style.visibility = "hidden";
+
+            ShowClose = false;
+            ShowMinimize = false;
+            ShowMaximize = false;
+
+            this.WindowState = WindowStateType.Maximized;
+
+            this.AllowMoveChange = false;
+            this.AllowSizeChange = false;
+        }
+    }
+}
